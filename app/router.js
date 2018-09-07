7b9076255b359e5ec0f8160ddc499a1eb9ec6c70
zulfo <33069559+zuxbrt@users.noreply.github.com>
2018-09-07 11:31:16 +0200

    added nested routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('rentals', function() {
+    // this.route('show');
+    this.route('show', { path: '/:rental_id' });
+  });
 });
 
 export default Router;
